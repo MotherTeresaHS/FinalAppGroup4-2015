403515c603b243a1cc84588023aeec71d58a9b54
LucasScuccato at codea-scm <LucasScuccato@codea-scm.aws.mapote.com>
2016-01-04 10:31:48 -0500

    added level tabs

--- tabs/MainGame.lua
+++ tabs/MainGame.lua
@@ -16,15 +16,14 @@ local levelsFive
 MainGame = class()
 
 function MainGame:init()
-    --sprite("Dropbox:Green Info Button")  
-    backButton = Button("Dropbox:Green Back Circle Button", vec2(65,710))
+    sprite("Dropbox:Green Info Button")  
+    backButton = Button("Dropbox:Blue Back Circle Button", vec2(65,710))
     secondWorldButton = Button("Dropbox:Green Forward Circle Button", vec2(970, 710))
-    levelsOne = Button("Dropbox:Green Info Button", vec2(50, 75))
-    levelsOne =  Button("Dropbox:Green Info Button", vec2(300, 200))
-    levelsTwo =  Button("Dropbox:Green Info Button", vec2(250, 200))
-    levelsThree =  Button("Dropbox:Green Info Button", vec2(300, 250))
-    levelsFour =  Button("Dropbox:Green Info Button", vec2(750, 200))
-    levelsFive =  Button("Dropbox:Green Info Button", vec2(800, 100))
+    levelsOne = Button("Dropbox:Green Info Button", vec2(100, 80))
+    levelsTwo =  Button("Dropbox:Green Info Button", vec2(335, 200))
+    levelsThree =  Button("Dropbox:Green Info Button", vec2(525, 400))
+    levelsFour =  Button("Dropbox:Green Info Button", vec2(735, 250))
+    levelsFive =  Button("Dropbox:Green Info Button", vec2(850, 50))
 
 end
 
@@ -33,9 +32,9 @@ function MainGame:draw()
     fill(0, 0, 0, 255)
     fontSize(40)
     font("AmericanTypewriter-Bold")
+    
     backButton:draw()
     secondWorldButton:draw()
-    levelsOne:draw()
     levelsOne:draw()
     levelsTwo:draw()
     levelsThree:draw()
@@ -45,13 +44,21 @@ function MainGame:draw()
 end
 
 function MainGame:touched(touch)
+    
     backButton:touched(touch)
     secondWorldButton:touched(touch)
     levelsOne:touched(touch)
+    levelsTwo:touched(touch)
+    levelsThree:touched(touch)
+    levelsFour:touched(touch)
+    levelsFive:touched(touch)
     if(backButton.selected == true) then
         Scene.Change("play")
     end
     if(secondWorldButton.selected == true) then
         Scene.Change("worldTwo")
+    end
+    if(levelsOne.selected == true) then
+        Scene.Change("levelsOne")
     end
 end
